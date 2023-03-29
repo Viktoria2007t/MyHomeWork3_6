518aa3f559b9c8a57ee06aef6ff5b5b4190d7e05
Роман <stroganova_elenka@mail.ru>
2023-03-29 05:45:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myhomework3_6/DrawingView.java
@@ -0,0 +1,147 @@
+package com.example.myhomework3_6;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingView extends View {
+
+    // создание полей
+    private Path drawPath; // путь для рисунка (траектория касания пальцем)
+    private Paint drawPaint, canvasPaint; // рисунок и краска на холсте
+    private int paintColor =  0xFF660000; // цвет по умолчанию
+    private Canvas drawCanvas; // canvas
+    private Bitmap canvasBitmap; // canvas bitmap
+    private float brushSize, lastBrushSize; // поля размера кисти (brushSize - размер кисти, lastBrushSize - последний размер кисти)
+
+    // создание дополнительных полей
+    private boolean erase = false; // поле флага действия стирания или рисования (ластик или кисть) false - кисть, true - ластик
+
+    // метод запуска нового документа
+    public void startNew() {
+        drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR); // очистка холста
+        invalidate(); // обновление окна отображения рисунка
+    }
+
+    // добавим сеттер поля флага действия стирания или рисования
+    public void setErase(boolean erase) {
+        this.erase = erase;
+        // параметр рисования (ластик или кисть)
+        if(erase) { // если выбор ластика
+            drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR)); // то назначается кисть очищения экрана
+        } else { // иначе
+            drawPaint.setXfermode(null); // обычная кисть
+        }
+    }
+
+    // конструктор родительского класса
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    // метод вызывается когда пользовательскому представлению присваивается размер
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888); // задание растрового изображения с шириной w и длиной h
+        drawCanvas = new Canvas(canvasBitmap); // экземпляр холста
+    }
+
+    // метод позволяющий классу функционировать как представление
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // холст и контур рисования
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    // необходимо добавить слушателя событий касания экрана
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //return super.onTouchEvent(event);
+
+        // извлечение координат касания экрана
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        // обработка движения касания экрана вниз, движения и вверх
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate(); // анулирование View
+        return true;
+    }
+
+    // метод настройки области рисования для взаимодействия
+    private void setupDrawing() {
+        drawPath = new Path(); // создание объекта траектории касания пальцем
+        drawPaint = new Paint(); // создание объекта рисунка и краски
+
+        drawPaint.setColor(paintColor); // установление цвета по умолчанию
+
+        // установление параметров кисти
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        // создание экземпляра размера кисти
+        brushSize = getResources().getInteger(R.integer.medium_size); // установление размера кисти по умолчанию
+        lastBrushSize = brushSize; // присваивание последней кисти значения кисти по умолчанию
+
+        drawPaint.setStrokeWidth(brushSize); // задание размера кисти
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG); // создание объекта класса Canvas
+    }
+
+    // метод изменения размера кисти
+    public void setBrushSize(float newSize) {
+        // изменение размера кисти
+        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                newSize, getResources().getDisplayMetrics()); // обновление размера кисти по параметру newSize
+        brushSize = pixelAmount; // переопределение brushSize
+        drawPaint.setStrokeWidth(brushSize); // задание в параметры кисти нового размера
+    }
+    // геттер и сеттер поля lastBrushSize (они потребуются потом в MainActivity)
+    public float getLastBrushSize() {
+        return lastBrushSize;
+    }
+    public void setLastBrushSize(float lastBrushSize) {
+        this.lastBrushSize = lastBrushSize;
+    }
+
+    // метод задания цвета
+    public void setColor(String newColor){
+        invalidate(); // признание представления недействительным
+        // далее разбираем и устанавливаем цвет для рисования
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+}
---
+++ app/src/main/java/com/example/myhomework3_6/MainActivity.java
@@ -0,0 +1,225 @@
+package com.example.myhomework3_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // создание полей
+    private DrawingView drawingView; // создание поля созданного нами класса
+    private ImageButton currPaint; // создание поля кнопки цвета краски в палитре
+    private ImageButton drawButton; // создание поля кнопки кисти рисования
+    private ImageButton eraseButton; // добавим поле кнопки ластика
+    private ImageButton newButton; // добавим поле кнопки нового рисунка
+    private float smallBrush, mediumBrush, largeBrush; // поля размеров кистей
+
+    // создание дополнительных полей
+    private ImageButton saveButton; // добавим поле кнопки сохранения рисунка
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // присваивание полям соответствующих id
+        drawingView = findViewById(R.id.drawing);
+        LinearLayout paintLayout = findViewById(R.id.paint_colors); // извлечение переменной контейнера по id
+
+        // присваивание дополнительным полям соответствующих id
+        smallBrush = getResources().getInteger(R.integer.small_size);
+        mediumBrush = getResources().getInteger(R.integer.medium_size);
+        largeBrush = getResources().getInteger(R.integer.large_size);
+        drawButton = findViewById(R.id.fabBrush);
+        eraseButton = findViewById(R.id.fabErase);
+        newButton = findViewById(R.id.fabAdd);
+        saveButton = findViewById(R.id.fabSave);
+
+        currPaint = (ImageButton) paintLayout.getChildAt(0); // получение первой кнопки
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed)); // при выборе данной кнопки она должна изменить вид в соответствии с ресурсом
+
+        // обработка нажатия кнопок
+        drawButton.setOnClickListener(this);
+        eraseButton.setOnClickListener(this);
+        newButton.setOnClickListener(this);
+        saveButton.setOnClickListener(this);
+    }
+
+    // метод выбора цвета
+    public void paintClicked(View view){
+
+        drawingView.setErase(false); // задание выбора кисти
+        drawingView.setBrushSize(drawingView.getLastBrushSize()); // установление размера кисти до использования ластика
+
+        // проверка выбранного цвета кнопки
+        if(view != currPaint){
+            // извлечение тега кнопки
+            ImageButton imgView = (ImageButton) view;
+            String color = view.getTag().toString();
+
+            // задание цвета по извлечённому тегу кнопки
+            drawingView.setColor(color);
+
+            // обновление пользовательского интерфейса
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed)); // присваивание новой формы кнопке (формы выбора)
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint)); // возврат прошлой кнопке обычной формы
+            currPaint = (ImageButton) view; // присваивание кнопки обратно view
+        }
+    }
+
+    // слушатель размера кисти
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.fabBrush){ // если выбрана кнопка рисования
+            // действия при нажатии кнопки кисти
+            final Dialog brushDialog = new Dialog(this, R.style.Dialog); // создадим диалоговое окно выбора размера кисти
+            brushDialog.setTitle("Размер кисти: "); // зададим заголовок диалогового окна
+            brushDialog.setContentView(R.layout.brush_chooser); // добавим ресурс диалогового окна
+            drawingView.setBrushSize(mediumBrush); // зададим средний размер кисти по умолчанию
+
+            ImageButton smallBtn = brushDialog.findViewById(R.id.small_brush); // создание кнопки маленького размера кисти
+            // добавим слушателя кнопки задания маленького размера кисти
+            smallBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawingView.setErase(false); // задание выбора кисти
+                    drawingView.setBrushSize(smallBrush); // задание малого размера кисти
+                    drawingView.setLastBrushSize(smallBrush); // задание последнего малого размера кисти
+                    brushDialog.dismiss(); // закрытие диалога
+                }
+            });
+
+            ImageButton mediumBtn = brushDialog.findViewById(R.id.medium_brush); // создание кнопки среднего размера кисти
+            // добавим слушателя кнопки задания среднего размера кисти
+            mediumBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawingView.setErase(false); // задание выбора кисти
+                    drawingView.setBrushSize(mediumBrush); // задание среднего размера кисти
+                    drawingView.setLastBrushSize(mediumBrush); // задание последнего среднего размера кисти
+                    brushDialog.dismiss(); // закрытие диалога
+                }
+            });
+
+            ImageButton largeBtn = brushDialog.findViewById(R.id.large_brush); // создание кнопки большого размера кисти
+            // добавим слушателя кнопки задания большого размера кисти
+            largeBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawingView.setErase(false); // задание выбора кисти
+                    drawingView.setBrushSize(largeBrush); // задание большого размера кисти
+                    drawingView.setLastBrushSize(largeBrush); // задание последнего большого размера кисти
+                    brushDialog.dismiss(); // закрытие диалога
+                }
+            });
+
+            brushDialog.show(); // отображение на экране данного диалога
+        } else if(view.getId() == R.id.fabErase) { // если выбрана кнопка ластика
+            // действия при нажатии кнопки ластика
+            final Dialog brushDialog = new Dialog(this, R.style.Dialog); // создадим диалоговое окно выбора размера ластика
+            brushDialog.setTitle("Размер ластика: "); // зададим заголовок диалогового окна
+            brushDialog.setContentView(R.layout.brush_chooser); // добавим ресурс диалогового окна
+            drawingView.setErase(true); // задание выбора ластика
+            drawingView.setBrushSize(mediumBrush); // зададим средний размер ластика по умолчанию
+
+            ImageButton smallBtn = brushDialog.findViewById(R.id.small_brush); // создание кнопки маленького размера ластика
+            // добавим слушателя кнопки задания маленького размера ластика
+            smallBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawingView.setErase(true); // задание выбора ластика
+                    drawingView.setBrushSize(smallBrush); // задание последнего малого размера ластика
+                    brushDialog.dismiss(); // закрытие диалога
+                }
+            });
+
+            ImageButton mediumBtn = brushDialog.findViewById(R.id.medium_brush); // создание кнопки среднего размера ластика
+            // добавим слушателя кнопки задания среднего размера ластика
+            mediumBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawingView.setErase(true); // задание выбора ластика
+                    drawingView.setBrushSize(mediumBrush); // задание последнего среднего размера ластика
+                    brushDialog.dismiss(); // закрытие диалога
+                }
+            });
+
+            ImageButton largeBtn = brushDialog.findViewById(R.id.large_brush); // создание кнопки большого размера ластика
+            // добавим слушателя кнопки задания большого размера ластика
+            largeBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawingView.setErase(true); // задание выбора ластика
+                    drawingView.setBrushSize(largeBrush); // задание последнего большого размера ластика
+                    brushDialog.dismiss(); // закрытие диалога
+                }
+            });
+
+            brushDialog.show(); // отображение на экране данного диалога
+        } else if(view.getId()==R.id.fabAdd){ // если выбрана кнопка нового документа
+            // код для создания нового документа
+            AlertDialog.Builder newDialog = new AlertDialog.Builder(this); // создание диалогового окна типа AlertDialog
+            newDialog.setTitle("Новый рисунок"); // заголовок диалогового окна
+            newDialog.setMessage("Новый рисунок (имеющийся будет удалён)?"); // сообщение диалога
+            newDialog.setPositiveButton("Да", new DialogInterface.OnClickListener(){ // пункт выбора "да"
+                public void onClick(DialogInterface dialog, int which){
+                    drawingView.startNew(); // старт нового рисунка
+                    dialog.dismiss(); // закрыть диалог
+                }
+            });
+            newDialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){ // пункт выбора "нет"
+                    dialog.cancel(); // выход из диалога
+                }
+            });
+            newDialog.show(); // отображение на экране данного диалога
+        } else if(view.getId()==R.id.fabSave){ // если выбрана кнопка сохранения документа
+            // код для сохранения документа
+            AlertDialog.Builder newDialog = new AlertDialog.Builder(this); // создание диалогового окна типа AlertDialog
+            newDialog.setTitle("Сохранить"); // заголовок диалогового окна
+            newDialog.setMessage("Сохранить рисунок?"); // сообщение диалога
+            newDialog.setPositiveButton("Да", new DialogInterface.OnClickListener(){ // пункт выбора "да"
+                public void onClick(DialogInterface dialog, int which){
+                    // код сохранения рисунка
+                    drawingView.setDrawingCacheEnabled(true); // сохраним кэш имеющегося рисунка
+
+                    // сохранение изображения в файл
+                    // (метод insertImage() записывает изображение в постоянную память устройства,
+                    // UUID.randomUUID().toString() - генерирует случайную строку для названия имени файла)
+                    String imageSaved = MediaStore.Images.Media.insertImage(
+                            getContentResolver(), drawingView.getDrawingCache(),
+                            UUID.randomUUID().toString()+".png", "drawing");
+
+                    // вывод тоста информации о сохранении рисунка
+                    if(imageSaved != null) { // если изображение сохранено, то вывод тоста об успешности сохранения
+                        Toast savedToast = Toast.makeText(getApplicationContext(),
+                                "Изображение успешно сохранено в галлерею!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    } else { // иначе, вывод тоста об неудачном сохранении
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                                "Сохранить изображение не удалось!", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+                    drawingView.destroyDrawingCache(); // удаление кэша рисунка
+                }
+            });
+            newDialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){ // пункт выбора "нет"
+                    dialog.cancel(); // выход из диалога
+                }
+            });
+            newDialog.show(); // отображение на экране данного диалога
+        }
+    }
+}
